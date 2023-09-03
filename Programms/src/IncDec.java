60ab479fdd6d3f352841a74ae5e5c8179e3f7d86
NIKHITA MN <NIKHITA MN@LAPTOP-HABG8J69>
2023-09-03 11:51:55 +0530

    IncDec.java

---
+++ Programms/src/IncDec.java
@@ -0,0 +1,29 @@
+
+public class IncDec {
+
+	public static void main(String[] args) {
+		int x=5;
+		x++;                          //6
+		++x;                         //6
+		System.out.println(x);
+		
+//		int x=5 , y;
+//		y=x++;        //first it will assign x value in y and increment so result will be 6 , 5
+//		y=++x;   //first increment and assign the value so result will be 6 , 6 
+//		System.out.println(x+ " " +y);
+		
+//		int x=5 , y=4 , z;
+//		z=2*x++ + 3 * ++x;   //31 ans
+//		z=2*x++ + 3 * ++y;   //25 ans
+//		System.out.println(z);
+		
+//		float x=3.5f;
+//      char x='A';
+//      byte x=5;
+//      x++;  //++x
+//      System.out.println(x);
+        
+
+	}
+
+}
